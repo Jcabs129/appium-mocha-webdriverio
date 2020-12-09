e80f1ced4502ee7d87e4edc3cb18b36b3ff8ccd0
James Cabeso <jamescabeso@Jamess-Personal-MacBook-Pro-2.local>
2020-12-09 19:05:23 +0000

    ipad setup for browser automation

---
+++ src/specs/mobileBrowser/mobileLinks.spec.js
@@ -0,0 +1,20 @@
+const LoginPage = require('./../../page-object/login.page');
+
+describe('forgot your password page', () => {
+
+  beforeEach(() => {
+    browser.url('https://account.dojo.tech/')
+    expect(browser).toHaveTitle('Dojo')
+    // expect(browser).toHaveUrlContaining('/login')
+  })
+    it('naviagte to apple dojo page', () => {
+      LoginPage.appleBtn()
+      // LoginPage.loginBtn()
+
+    });
+    it('naviagte to google dojo page', () => {
+      LoginPage.googleBtn()
+      // LoginPage.loginBtn()
+
+    });
+});
